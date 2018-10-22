66e6869687f7459b1ea56ba2a3e58750e58a0ca3
Samantha HILLEBRAND <shillebr@c1r3s3.wethinkcode.co.za>
2018-10-22 11:05:08 +0200

    Setting up registration and sign in

--- store_scripts/make_usertable.php
+++ store_scripts/make_usertable.php
@@ -1,17 +1,37 @@
 <?php
-require "connect_database.php";
 
-$sql = "CREATE TABLE 'users' (
-'id' int(11) NOT NULL auto_increment,
-'username' varchar(100) NOT NULL,
-'email' varchar(100) NOT NULL,
-'password' varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+$servername = "localhost";
+$username = "root";
+$password = "Samanthajh30";
+$my_db = "camagru_db";
 
-if (mysqli_query($conn, $sql))
-	echo "User Table created successfully.";
-else
-	echo "Error creating table " . mysqli_error($conn);
+$conn = mysqli_connect($servername, $username, $password);
 
-	mysqli_close($conn);
+$usr = "CREATE TABLE `users` (
+	`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`username` varchar(100) NOT NULL,
+	`email` varchar(100) NOT NULL,
+	`password` varchar(100) NOT NULL
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+$sql = "CREATE DATABASE $my_db";
+
+if (mysqli_query($conn, $sql) === TRUE){
+	{
+		echo "Database created successfully" . PHP_EOL;
+	}
+	$conn->close();
+	$conn = mysqli_connect($servername, $username, $password, $my_db);
+	if ($conn->connect_error) {
+		die("Connection failed after creation: " . $conn->connect_error);
+	}
+	if (mysqli_query($conn, $usr) === TRUE) {
+        echo "User table created successfully\n";
+    }
+}
+else {
+	echo "Error creating database: " . $conn->error;
+}
+
+return ($conn);
 ?>
