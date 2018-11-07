b622e6556293d22adc5b1d69814cb24136f0760d
sammyjh30 <sammyjh30@gmail.com>
2018-11-07 18:14:59 +0200

    Setting up upload

--- PDO_setup_grp/camera.php
+++ PDO_setup_grp/camera.php
@@ -5,30 +5,28 @@
 		<div class="input-group" style="width: 100%; text-align: center; align-content: center;">
 			<div id="status"></div>
 			<button onclick="takeSnapshot()" class="btn">Take pic</button>
-			<button onclick="savePic()" class="btn">Save</button> 
+			<!-- <button onclick="savePic()" class="btn">Save</button>  -->
 		</div>
 	</div>
 	<br/><br/>
 	<div id="container2" class="container2"></div>
-	<!-- <div id="container2" class="container2"> -->
-	<!-- <img class="snapshot"> -->
-	<!-- </div> -->
-	 <!-- Modal content -->
-    <div id= "modal-content" class="modal-content">
-        <div class="modal-header">
-            <span class="close">&times;</span>
-            <h2>Modal Header</h2>
-        </div>
-        <div class="modal-body">
-            <p>Some text in the Modal Body</p>
-            <p>Some other text...</p>
-			<img name="image" src="" id='modal-img'>
-        </div>
-        <div class="modal-footer">
-            <h3>Modal Footer</h3>
-			<button onclick="savePic()" class="btn">Save</button> 
-        </div>
-    </div>
+	<div id="myModal" class="modal">
+		<div class= "modal-content">
+		<div class="form-wrapper">
+			<form action="account/upload_snap.php" method="post" enctype="multipart/form-data">
+				<div class="modal-header">
+					<h2>Upload<span class="close">&times;</span></h2>
+				</div>
+				<img name="image" src="" id='modal-img'>
+				<p class="upload-font">Title: <input class="upload-box" required type="text" pattern="[^()/><\][\\\x22,;|]+" name="title"></p>
+				<textarea hidden name="base64" id="base64"></textarea>
+				<p class="upload-font">Description:  <input class="upload-box" required type="text" pattern="[^()/><\][\\\x22,;|]+" name="description"></p>
+				<br/>
+                <button onclick="savePic()" class="btn">Save</button>
+			</form>
+		</div>
+		</div>
+	</div>
 	<script>
 		var video = document.querySelector("#videoElement"), canvas;
 		var img = document.querySelector('snapshot') || document.createElement('snapshot');
@@ -58,14 +56,29 @@
 				save(new_img.src);
 			}
 			container2.insertBefore(new_img, container2.firstChild);
-			// document.getElementById("container2").innerHTML = "<img src="+img.src+">";
-			// document.getElementById("container2").innerHTML = "<img src="+img.src+" alt='snapshot' style='border:10px solid rgba(255, 255, 255, 0.2); width:60%; height:auto;'>";
 		}
 
 		function save(src) {
-			var modal = document.getElementById('modal-content');
+			var modal = document.getElementById('myModal');
 			var img = document.getElementById('modal-img');
 			var span = document.getElementsByClassName("close")[0];
+
+			img.src = src;
+			img.setAttribute("width", "100%");
+			document.getElementById('base64').value = src;
+			modal.style.display = "block";
+
+			// // When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+				modal.style.display = "none";
+			}
+
+			// // When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
 		}
 		// Save picture
 		function savePic(){
