2b23f9824c8a884576d2d41f6b9f58440fab7437
sammyjh30 <sammyjh30@gmail.com>
2018-11-15 11:26:50 +0200

    Infinite scroll fixed, now trying to update images

--- PDO_setup_grp/profile.php
+++ PDO_setup_grp/profile.php
@@ -1,97 +1,10 @@
-
-<!-- <div> -->
-	<!-- <div id="container_gal"> -->
-        <!-- <div id="scroll-content"> -->
-            <!-- <div>test div</div> -->
-        <!-- </div> -->
-	<!-- </div> -->
-    <!-- <div>       
-        <button onclick="expand()" class="btn">Next</button>
-    </div> -->
-<!-- </div> -->
-<script>
-// var numElementsToAdd = 1,
-//     offsetForNewContent = 1;
-
-// function checkInfiniteScroll(parentSelector, childSelector) {
-//     var lastDiv = document.querySelector(parentSelector + childSelector),
-//         lastDivOffset = lastDiv.offsetTop + lastDiv.clientHeight,
-//         pageOffset = window.pageYOffset + window.innerHeight;
-//         // field = getElementById("container"),
-//         // pageOffset = field.pageYOffset + field.innerHeight;
-
-//     if(pageOffset > lastDivOffset - offsetForNewContent ) {
-//         // for(var i = 0; i < numElementsToAdd; i++) {
-//             var newDiv = document.createElement("div");
-            <?php
-
-                // require('connect.php');
-                // $limit = 5;
-                // $offset = 0;
-                // $images = "SELECT `username`,`pic`,`pic_id` FROM pictures ORDER BY sub_datetime DESC LIMIT ".$limit." OFFSET ".$offset."";
-                // try {
-                //     $stmt = $pdo->prepare($images);
-                //     $stmt->execute();
-                //     $results = $stmt->fetchAll();
-                // } catch (Exception $ex) {
-                //     echo $ex->getMessage();
-                // }
-                // $offset = $offset + $limit;
-
-                // // $str = (count($results));
-                // if (count($results) > 0) {
-                //     $str = '\'<div class="row">';
-                //     foreach ($results as $res) {
-                //         $src = $res['pic'];
-                //         $str .= '<div class="column">';
-                //         $str .= '<img src = ';
-                //         $str .= $src;
-                //         $str .= ' style="width:90%;"/>';
-                //         $str .= '</div>';
-                //     }
-                //     $str .=  '</div>';
-                //     $str .=  '<br/>\'';
-                // }
-                // echo "</script><script type='text/javascript'>var string = $str;</script><script>"; 
-            ?>
-            // var string = <?php //echo $str ?>;
-//             newDiv.innerHTML = string;
-//             //  newDiv.innerHTML = "my awesome new div";
-//             document.querySelector(parentSelector).appendChild(newDiv);
-//         // }
-//         checkInfiniteScroll(parentSelector, childSelector);
-//     }
-// };
-
-// var lastScrollTime = Date.now(), 
-//     checkInterval = 50;
-
-// function update() {
-//   requestAnimationFrame(update);
-
-//   var currScrollTime = Date.now();
-//   if(lastScrollTime + checkInterval < currScrollTime) {
-//     checkInfiniteScroll("#scroll-content", "> div:last-child");
-//     lastScrollTime = currScrollTime;
-//   }
-// };
-
-// update();
-</script>
-
-<!-- <html>
-<head>
-<title>Infinite Scroll</title>
-</head>
-<body> -->
 <div>
   <h3>Infinite Scroll Example</h3>
-  <div id="scrollContent" style="overflow-y: scroll; height: 100px; width: 500px">
-    <div style="height: 300px; background-color: red">
+  <div id="scrollContent" style="overflow-y: scroll; height: 500px; width: 100%">
+    <div style="height: 500px; background-color: red">
     </div>
   <div>
 <div>
-<!-- </body> -->
 
 <script type="text/javascript">
   document.addEventListener('DOMContentLoaded',function () {
@@ -107,8 +20,14 @@
         <?php
             require('connect.php');
             $limit = 5;
-            $offset = 0;
-            $images = "SELECT `username`,`pic`,`pic_id` FROM pictures ORDER BY sub_datetime DESC LIMIT ".$limit." OFFSET ".$offset."";
+            global $offset;
+            if(empty($offset)){
+                $offset = 0;
+                // echo 'This line is printed, because the $var2 is empty.';
+            }
+            $start = $offset * $limit;
+            // $images = "SELECT `username`,`pic`,`pic_id` FROM pictures ORDER BY sub_datetime DESC LIMIT ".$limit." OFFSET ".$offset."";
+            $images = "SELECT `username`,`pic`,`pic_id` FROM pictures ORDER BY sub_datetime DESC LIMIT $start, $limit ";
             try {
                 $stmt = $pdo->prepare($images);
                 $stmt->execute();
@@ -116,7 +35,9 @@
             } catch (Exception $ex) {
                 echo $ex->getMessage();
             }
-            $offset = $offset + $limit;
+            $offset++;
+            $off = $offset;
+            echo "var off = $off;";
 
             // $str = (count($results));
             if (count($results) > 0) {
@@ -132,9 +53,9 @@
                 $str .=  '</div>';
                 $str .=  '<br/>\'';
             }
-            echo "</script><script type='text/javascript'>var string = $str;</script><script>"; 
+            echo "var string = $str;"; 
         ?>
-        var string = <?php //echo $str ?>;
+        elm.innerHTML += off;
         elm.innerHTML += string;
         // elm.innerHTML += '<div style="height: 300px; background-color: blue"> New Element Added </div>' ;
       }
